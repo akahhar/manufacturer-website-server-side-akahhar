9e45034d16ac92be2e95eb466461b4f30264e4c1
unknown <sm.hasanur121@gmail.com>
2022-05-26 21:12:22 +0600

    error fix last timea

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ async function run() {
     })
    
     // get user by email who is admin?
-    app.get("/admin/:email", verifyJWT ,async (req, res) => {
+    app.get("/y_admin/:email", verifyJWT ,async (req, res) => {
       const email = req.params.email;
      const user = await usersCollection.findOne({email : email});
      const isAdmin = user.role === "admin";
